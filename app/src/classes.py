01232a8c175201ca11421eaa6c9723d98e4236a5
Josh Daugherty <joshmd00@gmail.com>
2025-03-29 23:27:12 -0500

    Update classes.py

--- app/src/classes.py
+++ app/src/classes.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 class HL7Segment:
     def __init__(self, segment_name):
         self.segment_name = segment_name
@@ -14,13 +15,14 @@ class HL7Segment:
 
         subfields = {}
         if value:
-            subfields_values = value.split('^')
-            for i, subfield in enumerate(subfields_values, 1):
-                subfield_key = f"{field_name}.{i}"
-                repeating_values = subfield.split('~')
-                subfields[subfield_key] = repeating_values if len(repeating_values) > 1 else subfield
-        else:
-            subfields = None
+            if isinstance(value, str):
+                subfields_values = value.split('^')
+                for i, subfield in enumerate(subfields_values, 1):
+                    subfield_key = f"{field_name}.{i}"
+                    repeating_values = subfield.split('~')
+                    subfields[subfield_key] = repeating_values if len(repeating_values) > 1 else subfield
+
+
 
         self.fields[field_name] = {
             "Field Description": description,
@@ -52,10 +54,27 @@ class HL7Message:
 
     def get_message_control_id(self):
         if "MSH" in self.segments and "MSH-10" in self.segments["MSH"].fields:
-            return self.segments["MSH"].fields["MSH-10"]["Subfields"].get("MSH-10.1")
+            try:
+                return self.segments["MSH"].fields["MSH-10"]["Field Value"]
+            except Exception as e:
+                print(f"Error retrieving MSH-10: {e}",flush=True)
+                # print(f"MSH Segment: {self.segments['MSH'].fields}")
         return None
     
     def get_MRN(self):
         if "PID" in self.segments and "PID-3" in self.segments["PID"].fields:
-            return self.segments["PID"].fields["PID-3"]["Subfields"].get("PID-3.1", self.segments["PID"].fields["PID-3"]["Subfields"].get("PID-4.1"))
+            try:
+                subfields = [field for field in self.segments["PID"].fields["PID-3"]["Subfields"] if field.startswith("PID-3.")]
+                if not len(subfields) > 1:
+                    return self.segments["PID"].fields["PID-3"]["Subfields"].get(subfields[0])
+                else:   
+                    # find list with 'MR' in it
+                    for field in subfields:
+                        if 'MR' in self.segments["PID"].fields["PID-3"]["Subfields"][field]:
+                            return self.segments["PID"].fields["PID-3"]["Subfields"][field]
+
+            except Exception as e:
+                print(f"Error retrieving PID-3: {e}",flush=True)
+                # print(f"PID Segment: {self.segments['PID'].fields}")
+            
         return None
